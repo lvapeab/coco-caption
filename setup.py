cdefa596870b2393e527f002255f3976dca7ae73
lvapeab <lvapeab@gmail.com>
2017-04-26 18:35:21 +0200

    Setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='Coco Caption',
+      version='0.0',
+      description='Evaluation codes for MS COCO caption generation..',
+      url='https://github.com/lvapeab/coco-caption',
+      download_url='https://github.com/lvapeab/coco-caption/archive/master.zip',
+      packages=find_packages())
